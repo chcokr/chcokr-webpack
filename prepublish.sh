0906c2cbc2493ecedbf56538c2461b7abb95fa63
YJ Yang <chcokr@gmail.com>
2015-08-10 00:47:52 -0400

    Prepublish: more JSX files (bump 0.2.1)

---
+++ prepublish.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+babel StylesheetHotLoad.jsx > StylesheetHotLoad.js;
+babel style-hot/decorator.jsx > style-hot/decorator.js;
+babel style-hot/higher-order.jsx > style-hot/higher-order.js;
